e75b5b0d676cfad71657cf4ffb0225e4eea0cf0b
phlalx <phlalx@users.noreply.github.com>
2018-07-21 00:34:49 +0200

    added additional tests

--- test_viewservice/bin/main.ml
+++ test_viewservice/bin/main.ml
@@ -1,6 +1,7 @@
 open Core
 open Async
 
+
 let check cl p b n =
   let open Types in 
   let%map {viewnum; primary; backup} = Clerk.get cl in
@@ -20,7 +21,6 @@ let test_no_primary (cl1, _, _) =
   match%map Clerk.primary cl1 with
   | "" -> Log.Global.info "Test:  ... Passed"
   | _ -> failwith "Test: there was a primary too soon"
-
 
 let test_first_primary (cl1, _, _) =
   let action n =
@@ -60,22 +60,153 @@ let test_first_backup (cl1, cl2, _) : unit Deferred.t =
   let%map () = check cl1 cl1.Clerk.client cl2.Clerk.client 2 in
   Log.Global.info "Test: ... Passed"
 
+let test_primary_dies (cl1, cl2, _) : unit Deferred.t =
+  let open Types in
+  let%bind _ = Clerk.ping cl1 2 in 
+  let%bind view = Clerk.ping cl2 2 in 
+  let action n =
+    if n = 0 then
+      `Finished () |> return
+    else
+      let%bind view = Clerk.ping cl2 view.viewnum in 
+        if view.primary = cl2.Clerk.client 
+          && view.backup = "" then
+          `Finished () |> return
+        else 
+          let%map () = Clock.after Types.ping_interval in
+          `Repeat (n-1) 
+  in
+  Log.Global.info "Test: primary dies ..."; 
+  let n = Types.dead_ping * 2 in
+  let%bind () = Deferred.repeat_until_finished n action in
+  let%map () = check cl2 cl2.Clerk.client "" (view.viewnum + 1) in
+  Log.Global.info "Test: ... Passed"
+
+let test_restarted_backup (cl1, cl2, _) : unit Deferred.t =
+  let open Types in
+  let%bind view = Clerk.get cl2 in 
+  let%bind _ = Clerk.ping cl2 view.viewnum  in 
+  let action n =
+    if n = 0 then
+      `Finished () |> return
+    else
+      let%bind _ = Clerk.ping cl1 0  in 
+      let%bind view = Clerk.ping cl2 view.viewnum in 
+        if view.primary = cl2.Clerk.client && view.backup = cl1.client then
+          `Finished () |> return
+        else 
+          let%map () = Clock.after Types.ping_interval in
+          `Repeat (n-1) 
+  in
+  Log.Global.info "Test: Restarted server becomes backup ..."; 
+  let n = Types.dead_ping * 2 in
+  let%bind () = Deferred.repeat_until_finished n action in
+  let%map () = check cl2 cl2.Clerk.client cl1.Clerk.client (view.viewnum + 1) in
+  Log.Global.info "Test: ... Passed"
+
+let test_idle_third_server (cl1, cl2, cl3) : unit Deferred.t =
+  let open Types in
+  let%bind view = Clerk.get cl2 in 
+  let%bind _ = Clerk.ping cl2 view.viewnum  in 
+  let action n =
+    if n = 0 then
+      `Finished () |> return
+    else
+      let%bind _ = Clerk.ping cl3 0  in 
+      let%bind v = Clerk.ping cl1 view.viewnum in 
+        if v.primary = cl1.Clerk.client && v.backup = cl3.client then
+          `Finished () |> return
+        else 
+          let%map () = Clock.after Types.ping_interval in
+          `Repeat (n-1) 
+  in
+  Log.Global.info "Test: Idle third server becomes backup if primary fails..."; 
+  let n = Types.dead_ping * 2 in
+  let%bind () = Deferred.repeat_until_finished n action in
+  let%map () = check cl1 cl1.Clerk.client cl3.Clerk.client (view.viewnum + 1) in
+  Log.Global.info "Test: ... Passed"
+
+let test_restarted_primary (cl1, cl2, cl3) : unit Deferred.t =
+  let open Types in
+  let%bind view = Clerk.get cl1 in 
+  let%bind _ = Clerk.ping cl1 view.viewnum  in 
+  let action n =
+    if n = 0 then
+      `Finished () |> return
+    else
+      let%bind _ = Clerk.ping cl1 0  in 
+      let%bind _ = Clerk.ping cl3 view.viewnum in 
+      let%bind v = Clerk.get cl3 in 
+        if v.primary <> cl1.Clerk.client then
+          `Finished () |> return
+        else 
+          let%map () = Clock.after Types.ping_interval in
+          `Repeat (n-1) 
+  in
+  Log.Global.info "Test: Restarted primary treated as dead..."; 
+  let n = Types.dead_ping * 2 in
+  let%bind () = Deferred.repeat_until_finished n action in
+  let%map vy = Clerk.get cl3 in
+  assert (vy.primary = cl3.Clerk.client); (* TODO *)
+  Log.Global.info "Test: ... Passed"
+
+let set_up_view_with_three_as_primary (cl1, cl2, cl3) =
+  let open Types in
+  let action n =
+    if n = 0 then
+      `Finished () |> return
+    else
+      let%bind v = Clerk.get cl3  in 
+      let%bind _ = Clerk.ping cl3 v.viewnum in 
+      let%map () = Clock.after Types.ping_interval in
+      `Repeat (n-1) 
+  in
+  let n = Types.dead_ping * 3 in
+  let%bind () = Deferred.repeat_until_finished n action in
+  let%map vy = Clerk.get cl3 in
+  assert (vy.primary = cl3.Clerk.client && vy.backup = "") (* TODO *)
+
+let test_vs_waits_for_primary_to_ack_view (cl1, cl2, cl3) : unit Deferred.t =
+  let open Types in
+  let%bind view = Clerk.get cl1 in 
+  let action n =
+    if n = 0 then
+      `Finished () |> return
+    else
+      let%bind _ = Clerk.ping cl1 0  in 
+      let%bind _ = Clerk.ping cl3 view.viewnum in 
+      let%bind v = Clerk.get cl1 in 
+      if v.viewnum > view.viewnum then
+        `Finished () |> return
+      else 
+        let%map () = Clock.after Types.ping_interval in `Repeat (n-1) 
+  in
+  Log.Global.info "Test: Viewserver waits for primary to ack view..."; 
+  let n = Types.dead_ping * 3 in
+  let%bind () = Deferred.repeat_until_finished n action in
+  let%map () = check cl1 cl3.Clerk.client cl1.Clerk.client (view.viewnum + 1) in
+  Log.Global.info "Test: ... Passed"
+
 let test_viewservice port =
   let cl1 = Clerk.create "1" port in
   let cl2 = Clerk.create "2" port in
   let cl3 = Clerk.create "3" port in
   let ctx = cl1, cl2, cl3 in
-  test_no_primary ctx 
-  >>= fun () ->
-  test_first_primary ctx 
-  >>= fun () ->
-  test_first_backup ctx 
+  let%bind () = test_no_primary ctx in
+  let%bind () = test_first_primary ctx in
+  let%bind () = test_first_backup ctx in
+  let%bind () = test_primary_dies ctx  in
+  let%bind () = test_restarted_backup ctx  in
+  let%bind () = test_idle_third_server ctx  in
+  let%bind () = test_restarted_primary ctx  in
+  let%bind () = set_up_view_with_three_as_primary ctx in
+  test_vs_waits_for_primary_to_ack_view ctx
 
 let timeout = sec 1.0
 
 let process port () : unit Deferred.t = 
   Log.Global.set_level `Info;
-  let%map res = test_viewservice port in
+  let%map () = test_viewservice port in
   ()
 
 let () = 
--- viewservice/bin/main.ml
+++ viewservice/bin/main.ml
@@ -8,10 +8,22 @@ let terminate () : unit Deferred.t =
   Log.Global.info "VS: terminate";
   exit 0
 
-
-
 module State = struct 
+
+  let replicas = Hashtbl.Poly.create ()
+
+  let () = 
+    let incr_tick () = 
+      Log.Global.info "VS: tick";
+      Hashtbl.map_inplace replicas ~f:succ
+    in
+    Clock.every Types.ping_interval incr_tick
+
   type state = { mutable cur_view : int; views : Types.view Array.t }
+
+  let ping_replica (p:string) : bool = 
+    Hashtbl.set replicas ~key:p ~data:0; true 
+
 
   let state =
     let f i = Types.{ viewnum = i; primary = ""; backup = "" } in
@@ -19,6 +31,20 @@ module State = struct
       cur_view = 0;
       views = Array.init 100 ~f
     }
+
+  let fail replica =
+    let primary = state.views.(state.cur_view).primary in
+    let backup = state.views.(state.cur_view).backup in
+    state.cur_view <- state.cur_view + 1;
+    if replica = primary then ( 
+      Log.Global.info "VS: primary is dead";
+      state.views.(state.cur_view).primary <- backup;
+      state.views.(state.cur_view).backup <- ""
+    ) else if replica = backup then (
+      Log.Global.info "VS: backup is dead";
+      state.views.(state.cur_view).primary <- primary;
+      state.views.(state.cur_view).backup <- ""
+    ) 
 
   let setup_primary p =
     state.cur_view <- state.cur_view + 1;
@@ -32,7 +58,6 @@ module State = struct
 
   end
 
-
 let ping (viewnum, host) : Types.view = 
   let open State in
   Log.Global.info "VS: Received ping %d from %s" viewnum host;
@@ -42,7 +67,6 @@ let ping (viewnum, host) : Types.view =
   else assert false;
   Log.Global.info "VS: Ping return view %d" state.cur_view;
   state.views.(state.cur_view)
-
  
 let get () : Types.view =
   Log.Global.info "VS: Received get";
